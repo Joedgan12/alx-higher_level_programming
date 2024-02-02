fdb669ecacf1bd6502d946f905f8166bb33c6ca4
Joedgan12 <joepackins@gmail.com>
2024-02-02 05:44:13 -0800

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int joe;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (joe = 0; joe < size; joe++)
+		ht->array[joe] = NULL;
+
+	return (ht);
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+0x1A-hash_tables README file by Joepackins Owino
