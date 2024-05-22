0d27c5f339ea322ded83b71be7816f70f48a6a27
Joedgan12 <joepackins@gmail.com>
2024-05-22 21:41:29 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
+// Read a file and print the content
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
