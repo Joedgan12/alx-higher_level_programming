e64f0c2ee6afe7fbe77f2c6b353fa0a49dee17b7
Joedgan12 <joepackins@gmail.com>
2024-05-23 04:21:35 -0700

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,21 @@
 #!/usr/bin/node
 const fs = require('fs');
+let data1 = '';
+let data2 = '';
+try {
+  data1 = fs.readFileSync(process.argv[2], 'utf8');
+} catch (err) {
+  console.error(err);
+}
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+try {
+  data2 = fs.readFileSync(process.argv[3], 'utf8');
+} catch (err) {
+  console.error(err);
+}
+
+fs.writeFile(process.argv[4], data1 + data2, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+});
